700ac7ad17c2b0a74a0770d4cc36255a7f2b64ee
angus123456j <angus001.wolf@gmail.com>
2025-08-20 12:03:08 -0400

    Update authController.js

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -49,14 +49,20 @@ exports.login = async(req,res) => {
             accessToken: tokens.AccessToken,
             refreshToken: tokens.RefreshToken,
         };
-
-
         
         
-        await req.session.save((err) => { if (err) { console.error('Session save error:', err); 
-        return res.status(500).json({ error: 'Session save failed' }); } 
-        console.log("Session saved successfully"); 
-        res.status(200).json({ message: "Successfully logged in", user: req.session.userInfo, }); 
+       req.session.save(err => {
+            if (err) {
+                console.error('Session save error:', err);
+                return res.status(500).json({ error: 'Session save failed' });
+            }
+
+            console.log("Session saved successfully");
+            console.log("Session data:", req.session); // userInfo should appear here
+            res.status(200).json({ 
+                message: "Successfully logged in", 
+                user: req.session.userInfo 
+            });
         });
 
 
